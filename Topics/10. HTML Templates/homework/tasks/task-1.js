52fd1c7d72ef31da8076ee3c7b2715969b631a07
MonikaTzenova <monikatzenova@gmail.com>
2017-03-17 16:53:22 +0200

    Update task-1.js

--- Topics/10. HTML Templates/homework/tasks/task-1.js
+++ Topics/10. HTML Templates/homework/tasks/task-1.js
@@ -1,22 +1,32 @@
 /* globals $ */
 
 function solve() {
-  
-  return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
 
-    $(selector).html(template);
-  };
+    return function(selector) {
+
+   	 var template = '<table class="items-table">' +
+  '<thead>' +
+   '<tr>'+
+   '<th>#</th>' +
+     '{{#each headers}}'+
+		'<th>{{this}}</th>'+
+	'{{/each}}'+
+    '</tr>'+
+  '</thead>'+
+ '<tbody>'+
+ '{{#items}}' +
+ '<tr>'+	
+    '<td>{{@index}}</td>'+
+    '<td>{{col1}}</td>'+
+    '<td>{{col2}}</td>'+
+      '<td>{{col3}}</td>'+
+  '</tr>'+
+  	'{{/items}}' +
+  '</tbody>'+
+'</table>';
+
+        $(selector).html(template);
+    };
 };
 
 module.exports = solve;
